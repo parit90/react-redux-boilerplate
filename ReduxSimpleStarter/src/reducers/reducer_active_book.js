f89a8b9e15d9e845a19f00aff83a1a3015536351
parit sharma <parit.sharma@daffodilsw.com>
2017-05-29 19:54:36 +0530

    boilerplate code added

---
+++ ReduxSimpleStarter/src/reducers/reducer_active_book.js
@@ -0,0 +1,10 @@
+//state argument is not application state, only the state this reducer is responsible for
+
+//state=null initialize if app boots up first time, the state will be null, redux throw error if state is null
+export default function(state=null,action){
+    switch(action.type){
+        case 'BOOK_SELECTED': return action.payload;
+    }
+
+    return state;
+}
---
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+    return [
+        {title:'JavaScript Good Part'},
+        {title:'Harry Potter'},
+        {title:'The Dark tower'},
+        {title:'C/C++'}
+    ]
+
+}
